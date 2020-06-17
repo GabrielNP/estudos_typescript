227045e8dde3e8a7bdb0809f5fb3fc5768c774b8
GabrielNP <gabrielnp.2010@gmail.com>
2020-06-17 16:17:01 -0300

    Putting types in NegociacaoController methods

--- app/ts/controllers/NegociacaoController.ts
+++ app/ts/controllers/NegociacaoController.ts
@@ -1,8 +1,8 @@
 class NegociacaoController {
 
-    private _inputData;
-    private _inputQuantidade;
-    private _inputValor;
+    private _inputData : Element;
+    private _inputQuantidade : Element;
+    private _inputValor : Element;
 
     constructor() {
         this._inputData = document.querySelector('#data');
@@ -10,7 +10,8 @@ class NegociacaoController {
         this._inputValor = document.querySelector('#valor');
     }
 
-    adiciona = (event) => {
+    adiciona = (event : Event) => {
+        
         event.preventDefault();
 
         
