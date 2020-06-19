29c20d954296a8749a8cc21045e8ff4f716f7f37
GabrielNP <gabrielnp.2010@gmail.com>
2020-06-19 11:38:07 -0300

    Decorator logic

--- app/ts/helpers/decorators/logarTempoDeExecucao.ts
+++ app/ts/helpers/decorators/logarTempoDeExecucao.ts
@@ -6,7 +6,13 @@ export function logarTempoDeExecucao() {
 
         descriptor.value = function(...args: any[]) {
 
+            console.log('-----------------------')
+            console.log(`Parâmetros do método ${propertyKey}: ${JSON.stringify(args)}`);
+            const t1 = performance.now();
             const retorno = metodoOriginal.apply(this, args);
+            const t2 = performance.now();
+            console.log(`O retorno do método ${propertyKey} é ${JSON.stringify(retorno)}`);
+            console.log(`O método ${propertyKey} demorou ${t2-t1} ms`);
             return retorno;
         }
 
